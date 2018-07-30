448ac38e3c90be3aebdd34819b0e8f6dfeae626c
Mughees Khan <mugikhan@gmail.com>
2018-07-30 12:09:13 +0200

    Added pamphlet interaction

---
+++ Symptoms Please/Assets/ZoomControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomControl : MonoBehaviour {
+
+    public static string zoomActive = "n";
+
+    private void OnMouseDown()
+    {
+        if(zoomActive == "n"){
+            zoomActive = "y";
+        } else 
+        {
+            zoomActive = "n";
+        }
+    }
+}
---
+++ Symptoms Please/Assets/ZoomScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Camera>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ZoomControl.zoomActive == "y")
+        {
+            GetComponent<Camera>().enabled = true; 
+        } else
+        {
+            GetComponent<Camera>().enabled = false; 
+        }
+
+	}
+}
